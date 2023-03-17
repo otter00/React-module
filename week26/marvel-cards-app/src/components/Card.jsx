514a8182ec0053ac53b0970eee6f932597a40fc4
otter00 <ivlevanastia2001@mail.ru>
2023-03-17 13:49:08 +0300

    cards added

---
+++ week26/marvel-cards-app/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './card.css';
+
+class Card extends React.Component {
+    render() {
+        return {
+            // <div>
+            //     <><label htmlFor="name">Enter your name: </label><input type="text" id="name" /><p>Enter your HTML here</p></>
+            // </div>
+        }
+    }
+}
+
+export default Card;
