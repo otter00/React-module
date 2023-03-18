fd045b68d91fa61e5cc14af38382985ef2b5c5b9
otter00 <ivlevanastia2001@mail.ru>
2023-03-18 19:45:00 +0300

    classname edited

--- week26/marvel-cards-app/src/components/Card.jsx
+++ week26/marvel-cards-app/src/components/Card.jsx
@@ -1,23 +1,34 @@
 /* eslint-disabled */
 
-import React from 'react';
-import './card.css';
+import React from "react";
+import "./card.css";
 
 function Card(props) {
-        return (
-            <div class='gallery__card'>
-            <h2 class='card__name'>{props.name}</h2> 
-            <form action="#">
-            <h3>{props.universe}</h3>
-            <span class='alterego'>{props.alterego}</span> 
-            <span><p class='bold__text'>Специализация:</p>{props.occupation}</span>
-            <span><p class='bold__text'>Союзники:</p>{props.friends}</span>
-            <span><p class='bold__text'>Суперспособности:</p>{props.superpowers}</span> 
-            <div><img src={props.url} /></div>
-            <p>{props.info}</p>
-            </form>
-            </div>
-        );
+  return (
+    <div className="gallery__card">
+      <h2 className="card__name">{props.name}</h2>
+      <form action="#">
+        <h3>{props.universe}</h3>
+        <span className="alterego">{props.alterego}</span>
+        <span>
+          <p className="bold__text">Специализация:</p>
+          {props.occupation}
+        </span>
+        <span>
+          <p className="bold__text">Союзники:</p>
+          {props.friends}
+        </span>
+        <span>
+          <p className="bold__text">Суперспособности:</p>
+          {props.superpowers}
+        </span>
+        <div>
+          <img src={props.url} />
+        </div>
+        <p>{props.info}</p>
+      </form>
+    </div>
+  );
 }
 
 export default Card;
