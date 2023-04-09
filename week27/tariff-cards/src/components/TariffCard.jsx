d5bad025e75f2cf0e7f2752c61d2bcab77fb4cab
otter00 <ivlevanastia2001@mail.ru>
2023-04-09 21:46:59 +0300

    comments added

--- week27/tariff-cards/src/components/TariffCard.jsx
+++ week27/tariff-cards/src/components/TariffCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Tariffcard from './styles/tariffcard.module.scss';
-import cn from 'classnames';
 
 export default class TariffCard extends React.Component {
 
+    // super props is initial state for component (initially card is not clicked)
     constructor(props) {
         super(props);
         this.state = {
@@ -11,6 +11,7 @@ export default class TariffCard extends React.Component {
         };
     };
 
+    // func that changes the state of card by click anywhere inside the component
     handleChecked = () => {
         this.setState({
             isClicked: !this.state.isClicked,
@@ -18,12 +19,19 @@ export default class TariffCard extends React.Component {
     };
 
     render() {
-        let classCard='';
+
+        // props from JSON (App.jsx)
         const {fare, price, traffic, backHead, backBody, scaled } = this.props;
         
+        // check for the state of card
+        // helps to see how and where the state changes and when
         console.log(this.state.isClicked);
 
         return (
+            // hang up event upon the card wrapper
+            // after click call for the function
+            // and then make choosen (true) card scaled by hangin' up the props
+            // props is given by JSON as class with right style
             <div onClick={this.handleChecked} 
             className={(this.state.isClicked ? scaled : '')}>
             <div className={Tariffcard.card}>
